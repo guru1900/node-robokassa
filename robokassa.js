d0f8d2690fcaabba321dfa92b89559e75fecdee7
guru1 <guru1900@bk.ru>
2021-10-20 16:44:06 +0400

    добавление id заказа

--- robokassa.js
+++ robokassa.js
@@ -40,7 +40,7 @@ Robokassa.prototype.merchantUrl = function(order) {
 
 	if (order.lang) query.Culture = order.lang;
 
-	crcOpts.push(this.pass2);
+	crcOpts.push(this.pass1);
 
 
 	query.SignatureValue = hash(crcOpts.join(':'), this.hashType);
